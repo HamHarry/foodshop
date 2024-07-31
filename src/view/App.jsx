b9b009eac1b7c81d4b8d2ebad193f4d81040fdef
HamHarry <hamza_2641@hotmail.com>
2024-08-01 02:45:41 +0700

    Seach complete

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -8,93 +8,221 @@ import mockup from "../Mockup";
 
 function App() {
   const [list, setList] = useState(mockup);
+  const [listRef] = useState(list);
+  const [selectedType, setSelectedType] = useState("all");
+  const [selectedStock, setSelectedStock] = useState("all");
+  const [searchValue, setSearchValue] = useState("");
 
-  const Navbar = () => {
+  // แสดงข้อมูลสินค้า ======================================================
+  const WrapListCard = () => {
     return (
-      <div className="navbar">
-        <img src="/src/img/LOGO.jpg" alt="logo" />
-        <i className="fa-solid fa-basket-shopping"></i>
+      <div className="wrap-list-card">
+        {list.map((item, index) => {
+          const { name, price, img, score } = item;
+          return (
+            <div key={index} className="card">
+              <img src={img} alt={name} />
+              <div className="card-text">
+                <div className="text-name">
+                  <p>{name}</p>
+                  <div className="star">
+                    {Array.from({ length: 5 }).map((_, index) => {
+                      return score > index ? (
+                        <div key={index}>
+                          <i className="fa-solid fa-star"></i>
+                        </div>
+                      ) : (
+                        <div key={index}>
+                          <i className="fa-regular fa-star"></i>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className="text-price">
+                  <h2>{price} ฿</h2>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+  // Search and Button =====================================================
+  const ButtonMenu = () => {
+    return (
+      <div className="btn-menu">
+        <button
+          className={selectedType === "all" ? "is-btn-all" : "btn-all"}
+          onClick={() => {
+            const newlist = listRef.filter((item) => {
+              const statusStock = item.stock > 0 ? "in stock" : "out of stock";
+              const searchName = item.name.toLowerCase().includes(searchValue);
+              return (
+                (statusStock === selectedStock || selectedStock === "all") &&
+                searchName
+              );
+            });
+            setList(newlist);
+            setSelectedType("all");
+          }}
+        >
+          <p>All</p>
+        </button>
+        <button
+          className={selectedType === "food" ? "is-btn-food" : "btn-food"}
+          onClick={() => {
+            const newlist = listRef.filter((item) => {
+              const statusStock = item.stock > 0 ? "in stock" : "out of stock";
+              const searchName = item.name.toLowerCase().includes(searchValue);
+              return (
+                item.type === "food" &&
+                (statusStock === selectedStock || selectedStock === "all") &&
+                searchName
+              );
+            });
+            setList(newlist);
+            setSelectedType("food");
+          }}
+        >
+          <p>Food</p>
+        </button>
+        <button
+          className={
+            selectedType === "dessert" ? "is-btn-dessert" : "btn-dessert"
+          }
+          onClick={() => {
+            const newlist = listRef.filter((item) => {
+              const statusStock = item.stock > 0 ? "in stock" : "out of stock";
+              const searchName = item.name.toLowerCase().includes(searchValue);
+              return (
+                item.type === "dessert" &&
+                (statusStock === selectedStock || selectedStock === "all") &&
+                searchName
+              );
+            });
+            setList(newlist);
+            setSelectedType("dessert");
+          }}
+        >
+          <p>Dessert</p>
+        </button>
+        <button
+          className={selectedType === "drink" ? "is-btn-drink" : "btn-drink"}
+          onClick={() => {
+            const newlist = listRef.filter((item) => {
+              const statusStock = item.stock > 0 ? "in stock" : "out of stock";
+              const searchName = item.name.toLowerCase().includes(searchValue);
+              return (
+                item.type === "drink" &&
+                (statusStock === selectedStock || selectedStock === "all") &&
+                searchName
+              );
+            });
+            setList(newlist);
+            setSelectedType("drink");
+          }}
+        >
+          <p>Drink</p>
+        </button>
+      </div>
+    );
+  };
+  const SearchBar = () => {
+    return (
+      <div className="searchbar">
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder="Search..." onChange={handleChange} />
+      </div>
+    );
+  };
+  const ButtonStock = () => {
+    return (
+      <div className="btn-stock">
+        <button
+          className={
+            selectedStock === "in stock" ? "is-btn-in-stock" : "btn-in-stock"
+          }
+          onClick={() => {
+            const newlist = listRef.filter((item) => {
+              return (
+                item.stock > 0 &&
+                (item.type === selectedType || selectedType === "all")
+              );
+            });
+            setList(newlist);
+            setSelectedStock("in stock");
+          }}
+        >
+          <p>In stock</p>
+        </button>
+        <button
+          className={
+            selectedStock === "out of stock"
+              ? "is-btn-out-of-stock"
+              : "btn-out-of-stock"
+          }
+          onClick={() => {
+            const newlist = listRef.filter((item) => {
+              return (
+                item.stock <= 0 &&
+                (item.type === selectedType || selectedType === "all")
+              );
+            });
+            setList(newlist);
+            setSelectedStock("out of stock");
+          }}
+        >
+          <p>Out of stock</p>
+        </button>
       </div>
     );
   };
 
-  const Footer = () => {
-    return (
-      <div className="footer">
-        <p>Developer By HamHarry</p>
-      </div>
-    );
+  // fucntion Seach =====================================================
+  const handleChange = (e) => {
+    const value = e.target.value.toLowerCase();
+    const newlist = listRef.filter((item) => {
+      const searchname = item.name.toLowerCase().includes(value);
+      const searchtype = item.type === selectedType || selectedType === "all";
+      const stock = item.stock > 0 ? "in stock" : "out of stock";
+      const searchstock = stock === selectedStock || selectedStock === "all";
+      return searchname && searchtype && searchstock;
+    });
+    setList(newlist);
+    setSearchValue(value);
   };
 
   return (
     <div>
-      <Navbar />
+      <div className="navbar">
+        <img
+          src="/src/img/LOGO.jpg"
+          alt="logo"
+          onClick={() => {
+            return window.location.reload(false);
+          }}
+        />
+        <i className="fa-solid fa-basket-shopping"></i>
+      </div>
       <div className="container">
         <div className="list-card">
           <div className="search">
-            <div className="btn-menu">
-              <button className="btn-all">
-                <p>All</p>
-              </button>
-              <button className="btn-food">
-                <p>Food</p>
-              </button>
-              <button className="btn-dessert">
-                <p>Dessert</p>
-              </button>
-              <button className="btn-drink">
-                <p>Drink</p>
-              </button>
-            </div>
-            <i className="fa-solid fa-magnifying-glass"></i>
-            <input type="text" />
-            <div className="btn-stock">
-              <button className="btn-in-stock">
-                <p>In stock</p>
-              </button>
-              <button className="btn-out-of-stock">
-                <p>Out of stock</p>
-              </button>
-            </div>
+            {ButtonMenu()}
+            {SearchBar()}
+            {ButtonStock()}
           </div>
-          <div className="wrap-list-card">
-            {list.map((item, index) => {
-              const { name, price, img, score } = item;
-              return (
-                <div key={index} className="card">
-                  <img src={img} alt={name} />
-                  <div className="card-text">
-                    <div className="text-name">
-                      <p>{name}</p>
-                      <div className="star">
-                        {Array.from({ length: 5 }).map((_, index) => {
-                          return score > index ? (
-                            <div key={index}>
-                              <i className="fa-solid fa-star"></i>
-                            </div>
-                          ) : (
-                            <div key={index}>
-                              <i className="fa-regular fa-star"></i>
-                            </div>
-                          );
-                        })}
-                      </div>
-                    </div>
-                    <div className="text-price">
-                      <h2>{price}$</h2>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          {WrapListCard()}
         </div>
         <div className="list-pay">
           <div className="cart"></div>
           <div className="payment"></div>
         </div>
       </div>
-      <Footer />
+      <div className="footer">
+        <p>Developer By HamHarry</p>
+      </div>
     </div>
   );
 }
